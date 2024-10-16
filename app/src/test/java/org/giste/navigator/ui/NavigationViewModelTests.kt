586fa2a589b58e64e2ff76dba01e4a756665715d
Giste <giste.trappiste@gmail.com>
2024-10-16 16:15:36 +0200

    Use nested tests

--- app/src/test/java/org/giste/navigator/ui/NavigationViewModelTests.kt
+++ app/src/test/java/org/giste/navigator/ui/NavigationViewModelTests.kt
@@ -2,71 +2,97 @@ package org.giste.navigator.ui
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
-
+@DisplayName("Tests for NavigationViewModel")
 class NavigationViewModelTests {
     private val viewModel = NavigationViewModel()
 
-    @Test
-    fun setPartial_ifItIsInRange_shouldUpdateIt() {
-        viewModel.setPartial("123,45")
+    @DisplayName("Given minimum partial (0)")
+    @Nested
+    inner class PartialIsMin {
+        @Test
+        fun `increase() should add 10 meters`() {
+            viewModel.increasePartial()
 
-        assertEquals(123450, viewModel.uiState.partial)
-    }
+            assertEquals(10, viewModel.uiState.partial)
+        }
 
-    @Test
-    fun setPartial_ifItIsOutOfRange_ShouldThrowException() {
-        assertThrows(IllegalArgumentException::class.java) {
-            viewModel.setPartial("1000,00")
+        @Test
+        fun `decrease() should not change partial`(){
+            viewModel.decreasePartial()
+
+            assertEquals(0, viewModel.uiState.partial)
         }
     }
 
-    @Test
-    fun increasePartial_ifPartialIsMinorThan999Dot99_shouldAddOneUnit() {
-        viewModel.increasePartial()
+    @DisplayName("Given maximum partial (>=999990)")
+    @Nested
+    inner class PartialIsMax {
+        @BeforeEach
+        fun setup() {
+            viewModel.setPartial("999,99")
+        }
 
-        assertEquals(10, viewModel.uiState.partial)
+        @Test
+        fun `increase() should not change partial`() {
+            viewModel.increasePartial()
+
+            assertEquals(999990, viewModel.uiState.partial)
+        }
+
+        @Test
+        fun `decrease() should subtract 10 meters`() {
+            viewModel.decreasePartial()
+
+            assertEquals(999980, viewModel.uiState.partial)
+        }
     }
 
-    @Test
-    fun increasePartial_ifPartialIs999Dot99_shouldRemainTheSame() {
-        viewModel.setPartial("999,99")
-        viewModel.increasePartial()
+    @DisplayName("setPartial()")
+    @Nested
+    inner class SetPartialTests {
+        @Test
+        fun `when it's in 0-999 should update partial`() {
+            viewModel.setPartial("123,45")
 
-        assertEquals(999990, viewModel.uiState.partial)
+            assertEquals(123450, viewModel.uiState.partial)
+        }
+
+        @Test
+        fun `when it's out of range should throw IllegalArgumentException`(){
+            assertThrows(IllegalArgumentException::class.java) {
+                viewModel.setPartial("1000,00")
+            }
+        }
     }
 
-    @Test
-    fun decreasePartial_ifPartialGreaterThanZero_shouldSubtractOneUnit() {
-        viewModel.setPartial("123,45")
-        viewModel.decreasePartial()
+    @DisplayName("Given partial and total > 0")
+    @Nested
+    inner class ResetTests {
+        @BeforeEach
+        fun setup() {
+            viewModel.setPartial("123,45")
+            //TODO("Can I set total value?")
+        }
 
-        assertEquals(123440, viewModel.uiState.partial)
+        @Test
+        fun `resetPartial() should set partial to 0`() {
+            viewModel.resetPartial()
+
+            assertEquals(0, viewModel.uiState.partial)
+        }
+
+        @Test
+        fun `resetAll() should set partial and total to 0`() {
+            viewModel.resetAll()
+
+            assertEquals(0, viewModel.uiState.partial)
+            assertEquals(0, viewModel.uiState.total)
+        }
     }
 
-    @Test
-    fun decreasePartial_ifPartialIsZero_shouldRemainZero() {
-        viewModel.decreasePartial()
-
-        assertEquals(0, viewModel.uiState.partial)
-    }
-
-    @Test
-    fun resetPartial_shouldSetPartialToZero() {
-        viewModel.setPartial("123,45")
-        viewModel.resetPartial()
-
-        assertEquals(0, viewModel.uiState.partial)
-    }
-
-    @Test
-    fun resetAll_shouldSetTotalAndPartialToZero() {
-        viewModel.setPartial("123,45")
-        viewModel.resetAll()
-
-        assertEquals(0, viewModel.uiState.partial)
-        //TODO("Check total")
-        assertEquals(0, viewModel.uiState.total)
-    }
 }
